faadc97469b2a2c1c96d8571b242a8bf6d00876f
samuel-pnj <samuelperezgamez@outlook.com>
2022-06-29 21:36:26 +0200

    primer commit

---
+++ src/app/contador.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector: 'app-contador',
+    template: `
+    <h1>{{  titulo  }}</h1>
+    <h3>la base es: <strong>{{  base  }}</strong></h3>
+    <button (click)=" acumular(base)"> + {{  base  }} </button>
+    
+    <span>{{  numero  }}</span>
+    
+    <button (click)=" acumular(-base)"> - {{ base }}</button>
+    `
+})
+export.class ContadorComponent{
+        
+    }
